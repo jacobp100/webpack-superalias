0c1a3f58fb588cac12c049024bbf23a9f73d5180
Jacob Parker <jacobparker1992@gmail.com>
2016-06-06 19:30:14 +0100

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+function SuperAlias(fn) {
+  this.fn = fn;
+}
+
+SuperAlias.prototype.apply = function apply(compiler) {
+  var fn = this.fn;
+
+  compiler.resolvers.normal.apply({
+    apply: function applyResolver(resolver) {
+      resolver.plugin('resolve', function resolve(context, request) {
+        request.path = fn(request); // eslint-disable-line
+      });
+    }
+  });
+};
+
+module.exports = SuperAlias;
